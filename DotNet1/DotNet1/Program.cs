74d38e6a2d43eed5be189213f7f94072c3f4823b
feridnasirli <100508229+feridnasirli@users.noreply.github.com>
2022-02-27 15:20:11 +0400

    New Project started

---
+++ DotNet1/DotNet1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DotNet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = "ZXCzxc123";
+            Console.WriteLine("Hello Farid!");
+            Console.Write("Please enter password for start ");
+            string command  = Console.ReadLine();
+
+            if (command == pass)
+            {
+                Console.WriteLine("Program started successfully");
+            }
+            else
+            {
+                Console.WriteLine("Password is incorrect. Please reenter password again.");
+            }
+
+
+
+        }
+    }
+}
