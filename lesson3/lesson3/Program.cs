5c261a4925f64457c17135e9aeb60b8ea00720a6
feridnasirli <100508229+feridnasirli@users.noreply.github.com>
2022-02-28 14:32:46 +0400

    lesson 3

--- lesson3/lesson3/Program.cs
+++ lesson3/lesson3/Program.cs
@@ -6,7 +6,16 @@ namespace lesson3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            short var1 = short.Parse(Console.ReadLine());
+            short var2 = short.Parse(Console.ReadLine());
+            int sum = var1 + var2;
+            Console.WriteLine(sum);
+            double var3 = 589.890;
+            float var4 = 510.11f;
+            double sum2 = var3 + var4;
+            Console.WriteLine(sum2);
+
+
         }
     }
 }
